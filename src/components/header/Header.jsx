18dd91e11f0c10fcbb59749f371943a5d76a2e6d
manojpalyam0815 <palyammanojkumarmn@gmail.com>
2022-09-21 07:23:31 +0530

    manu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Palyam Manoj</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
+        <h5 className="text-light">Full Stack Web Developer</h5>
         <CTA />
         <HeaderSocial />
         <div className="me">
